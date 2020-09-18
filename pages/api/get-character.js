691f180f01403b2f7108c688846c54534feed5f7
Ashley Brown <ashleybrown@ashleys-mbp-2.home>
2020-09-18 16:07:13 +0100

    API changes

--- pages/api/get-character.js
+++ pages/api/get-character.js
@@ -1,34 +1,26 @@
-const MongoClient = require("mongodb").MongoClient
+const MongoClient = require("mongodb").MongoClient;
 
 export default async function (req, res) {
-    const data = JSON.parse(req.body)
+  const data = JSON.parse(req.body);
 
-    try {
-        const client = new MongoClient(process.env.MONGO_DB_URL, {useUnifiedTopology: true})
-        await client.connect()
+  try {
+    const client = new MongoClient(process.env.MONGO_DB_URL, {
+      useUnifiedTopology: true,
+    });
+    await client.connect();
 
-        const character = await client.db(process.env.MONGO_DB_NAME).collection(data.name.toLowerCase()).find({"name": data.name.toLowerCase()}).toArray()
+    const character = await client
+      .db(process.env.MONGO_DB_NAME)
+      .collection(data.name.toLowerCase())
+      .find({ name: data.name.toLowerCase() })
+      .toArray();
 
-        await client.close()
+    await client.close();
 
-        return res.status(200).json(character[0])
-    }
-    catch {
-        return res.status(500).json(
-            {
-                "name": "bob", 
-                "class": "adventurer", 
-                "gold": 500, 
-                "inventory": {
-                    "head": null,
-                    "body": null,
-                    "r-hand": null,
-                    "l-hand": null,
-                    "legs": null,
-                    "feet": null,
-                    "items": {"potion": 1}
-                 } 
-            }
-        )
-    }
- }
+    return res.json(character[0]);
+  } catch (error) {
+    console.log("api error: ", error);
+    //keep the API pure, and handle failures at the requester
+    return res.status(500).end();
+  }
+}
--- pages/protected.js
+++ pages/protected.js
@@ -1,53 +1,70 @@
-import { useEffect } from "react"
-import fetch from 'isomorphic-unfetch'
-import { useSession, getSession } from 'next-auth/client'
-import Layout from '../components/layout'
-import AccessDenied from '../components/access-denied'
+import { useEffect } from "react";
+import fetch from "isomorphic-unfetch";
+import { useSession, getSession } from "next-auth/client";
+import Layout from "../components/layout";
+import AccessDenied from "../components/access-denied";
 
 export default ({ character }) => {
-  const [ session, loading ] = useSession()
+  const [session, loading] = useSession();
 
-  if (!session) { return <AccessDenied/> }
+  if (!session) {
+    return <AccessDenied />;
+  }
 
   useEffect(() => {
-    localStorage.setItem('character', JSON.stringify(character))
-  }, [])
+    localStorage.setItem("character", JSON.stringify(character));
+  }, []);
 
   return (
     <Layout>
       <h1>Protected Page</h1>
-      <p>This page is protected using server side protection, you must be signed in to access it.</p>
+      <p>
+        This page is protected using server side protection, you must be signed
+        in to access it.
+      </p>
       <p>You can view this page because you are signed in.</p>
-
     </Layout>
-  )
-}
+  );
+};
 
 export async function getServerSideProps(context) {
-
   // If you need to make calls to a service (e.g. an API or database) to make
   // data avalible only to authenticated users, you can do that here by checking
   // the session object is not null or by accessing the contents of session.user
-  const session = await getSession(context)
-  let character
+  const session = await getSession(context);
+  let character;
 
-  // TODO: Remove hardcoded name on fetch! 
+  // TODO: Remove hardcoded name on fetch!
   try {
-    const response = await fetch('http://localhost:3000/api/get-character', {method: "POST", body: JSON.stringify({"name": 'theflyingdev'})})
+    const response = await fetch("http://localhost:3000/api/get-character", {
+      method: "POST",
+      body: JSON.stringify({ name: "theflyingdev" }),
+    });
 
-    const res = await response.json()
-
-    if (res.status === 200) {
-      character = JSON.stringify(res)
+    //Fetch doesn't throw errors on status codes like 500, so throw manually
+    if (response.status === 500) {
+      throw new Error(response.status);
     }
-  } 
-  catch (error) {
-    console.log(error)
-
-    character = JSON.stringify(error)
+    character = await response.json();
+  } catch (error) {
+    console.log("get-character error: ", error);
+    character = JSON.stringify({
+      name: "bob",
+      class: "adventurer",
+      gold: 500,
+      inventory: {
+        head: null,
+        body: null,
+        "r-hand": null,
+        "l-hand": null,
+        legs: null,
+        feet: null,
+        items: { potion: 1 },
+      },
+    });
   }
 
   return {
-    props: { session, character }
-  }
+    props: { session, character },
+  };
 }
